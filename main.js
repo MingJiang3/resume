96ca30a0f7cea86cc135b5837af010fafb6ceedc
MingJiang3 <457918455@qq.com>
2018-08-13 23:27:18 +0800

    暂时储存

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-window.onscroll = function(scroll){
-    if(window.scrollY > 0){
-      topNavBar.classList.add('sticky')
-    }else{
-      topNavBar.classList.remove('sticky')
+window.onscroll = function (scroll) {
+    if (window.scrollY > 0) {
+        topNavBar.classList.add('sticky')
+    } else {
+        topNavBar.classList.remove('sticky')
     }
-  }
+}
+let aTags = document.getElementsByClassName('menuTigger')
+//  console.log(aTags);
+for (let i = 0; i < aTags.length; i++) {
+    aTags[i].onmouseenter = function () {
+        console.log('onmouse')
+    //     let a = x.currentTarget
+    //     let brother = a.nextSibling
+    //     while (brother.nodeType === 3) {
+    //         brother = brother.nextSibling
+    //     }
+    //     console.log('brother')
+    }
+    aTags[i].onmouseleave = function () {
+        console.log('onmouseleave')
+    }
+}
