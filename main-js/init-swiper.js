4ea1a277d7327a88fdcf9b6d41bd79123b2cf23a
MingJiang3 <457918455@qq.com>
2018-09-04 22:09:32 +0800

    改成MVC

--- main-js/init-swiper.js
+++ main-js/init-swiper.js
@@ -1,5 +1,19 @@
-var mySwiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: { el: '.swiper-pagination', },
-    navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev', }
-})
+!function () {
+    var view = document.querySelector('.swiper-container')
+    var controller = {
+        view: null,
+        swiper: null,
+        swiperOptions: { loop: true, pagination: { el: '.swiper-pagination', }, navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev', } },
+        init: function (view) {
+            this.view = view
+            this.initSwiper()
+        },
+        initSwiper: function () {
+            this.swiper = new Swiper(
+                this.view.querySelector('.swiper-container'),
+                this.swiperOptions
+            )
+        }
+    }
+    controller.init(view)
+}.call()
--- main-js/message.js
+++ main-js/message.js
@@ -1,14 +1,16 @@
-var APP_ID = 'LvGf85PnKyDeY32xFvDBFpRA-gzGzoHsz';
-var APP_KEY = 'I4j1SbFgMqCV3rC7n4bRdm3v';
-
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
-
-var TestObject = AV.Object.extend('TestObject');
-var testObject = new TestObject();
-testObject.save({
-  words: 'Hello World!'
-}).then(function(object) {
-})
+!function() {
+  var APP_ID = 'LvGf85PnKyDeY32xFvDBFpRA-gzGzoHsz';
+  var APP_KEY = 'I4j1SbFgMqCV3rC7n4bRdm3v';
+  
+  AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+  });
+  
+  var TestObject = AV.Object.extend('TestObject');
+  var testObject = new TestObject();
+  testObject.save({
+    words: 'Hello World!'
+  }).then(function(object) {
+  })
+}.call()
