4b5d0b5f475f26de51aeeced5d9a6dbb5f2ff86c
MingJiang3 <457918455@qq.com>
2018-09-05 00:37:45 +0800

    00

--- main-js/message.js
+++ main-js/message.js
@@ -1,16 +1,40 @@
-!function() {
+
   var APP_ID = 'LvGf85PnKyDeY32xFvDBFpRA-gzGzoHsz';
   var APP_KEY = 'I4j1SbFgMqCV3rC7n4bRdm3v';
-  
   AV.init({
     appId: APP_ID,
     appKey: APP_KEY
   });
-  
-  var TestObject = AV.Object.extend('TestObject');
-  var testObject = new TestObject();
-  testObject.save({
-    words: 'Hello World!'
-  }).then(function(object) {
+
+  var query = new AV.Query('Todo');
+  query.find().then(function (todos) {
+    todos.forEach(function(todo) {
+      todo.set('status', 1);
+    });
+    return AV.Object.saveAll(todos);
+  }).then(function(todos) {
+    // 更新成功
+  }, function (error) {
+    // 异常处理
+  });
+
+
+
+
+
+
+  let myForm = document.querySelector('#postMessageForm')
+  myForm.addEventListener('submit',function(e){
+    e.preventDefault()
+    let content = myForm.querySelector('input[name=content]').value
+    var Message = AV.Object.extend('message');
+    var message = new Message();
+    message.save({
+      'content':content
+    }).then(function(object) {
+      alert('提交成功')
+    })
   })
-}.call()
+
+  
+
