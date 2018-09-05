a8e137ad850c744751ce5b373dd58f2bfdec447f
MingJiang3 <457918455@qq.com>
2018-09-05 15:25:06 +0800

    message初步使用MVC

--- main-js/message.js
+++ main-js/message.js
@@ -1,40 +1,71 @@
+!function () {
+  var view = document.querySelector('section.message')
+  var model = {
+    init: function () {
+      var APP_ID = 'LvGf85PnKyDeY32xFvDBFpRA-gzGzoHsz';
+      var APP_KEY = 'I4j1SbFgMqCV3rC7n4bRdm3v';
+      AV.init({ appId: APP_ID, appKey: APP_KEY })
+    },
+    // 获取数据
+    fetch: function () {
+      var query = new AV.Query('message');
+      return query.find()
+    },
+    //创建数据
+    save: function (name, content) {
+      var Message = AV.Object.extend('message');
+      var message = new Message();
+      message.save({
+        'content': content,
+        'name': name
+      })
+    }
+  }
+  var controller = {
+    view: null,
+    model: null,
+    messageList: null,
+    init: function (view, model) {
+      this.view = view
+      this.model = model
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.model.init()
+      this.loadMessages()
+      this.bindEvents()
+    },
+    loadMessages: function () {
+      this.model.fetch().then((message) => {
+        let array = message.map((item) => item.attributes)
+        array.forEach((item) => {
+          let li = document.createElement('li')
+          li.innerText = `${item.name}:${item.content}`
+          this.messageList.appendChild(li)
+        })
+      }
+      )
+    },
+    bindEvents: function () {
+      this.form.addEventListener('submit', function (e) {
+        e.preventDefault()
+        this.saveMessage
+      }.bind(this))
+    },
+    saveMessage: function () {
+      let myForm = this.form
+      let content = myForm.querySelector('input[name=content]').value
+      let name = myForm.querySelector('input[name=name]').value
+      this.model.save(name, content).then(function (object) {
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+        myForm.querySelector('input[name=content]').value = ''
+      })
+    }
+  }
+  controller.init(view, model)
+}.call()
 
-  var APP_ID = 'LvGf85PnKyDeY32xFvDBFpRA-gzGzoHsz';
-  var APP_KEY = 'I4j1SbFgMqCV3rC7n4bRdm3v';
-  AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-  });
 
-  var query = new AV.Query('Todo');
-  query.find().then(function (todos) {
-    todos.forEach(function(todo) {
-      todo.set('status', 1);
-    });
-    return AV.Object.saveAll(todos);
-  }).then(function(todos) {
-    // 更新成功
-  }, function (error) {
-    // 异常处理
-  });
-
-
-
-
-
-
-  let myForm = document.querySelector('#postMessageForm')
-  myForm.addEventListener('submit',function(e){
-    e.preventDefault()
-    let content = myForm.querySelector('input[name=content]').value
-    var Message = AV.Object.extend('message');
-    var message = new Message();
-    message.save({
-      'content':content
-    }).then(function(object) {
-      alert('提交成功')
-    })
-  })
-
-  
 
